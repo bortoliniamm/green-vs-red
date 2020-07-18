c0fa7f5c542570f23cc69db15dad914227689d48
bortoliniamm <bortoliniamm@gmail.com>
2020-07-18 12:13:04 +0200

    Added variable array size feature

--- src/App.js
+++ src/App.js
@@ -5,57 +5,67 @@ import ColumnInput from './components/ColumnInput';
 
 function App() {
 
+  let auxArr=[];
+  let initialArr=[];
+  let flag=0;
+
+  for(let i=0; i<9; i++){
+    if(flag===0){
+      initialArr.push(flag);
+      flag=1;
+    }else{
+      initialArr.push(flag);
+      flag=0;
+    }
+  }
+
   const [noRows, setNoRows] = useState(3);
   const [noColumns, setNoColumns] = useState(3);
-  const [gridArr, setGridArr] = useState([]);
-  const [gridArrSize, setGridArrSize] = useState(9);
-
+  const [gridArr, setGridArr] = useState(initialArr);
+  
   const handleRowChange = (value) => {
     setNoRows(value);
   }; 
-
+  
   const handleColumnChange = (value) => {
     setNoColumns(value);
   }; 
-
+  
   useEffect(() => {
-    console.log(`Rows ${noRows}`);
-    console.log(`Cols ${noColumns}`);
-    setGridArrSize(noRows*noColumns);
+    
+    flag = 0;
+    
+    for(let i=0; i<(noRows*noColumns); i++){
+      if(flag===0){
+        auxArr.push(flag);
+        flag=1;
+      }else{
+        auxArr.push(flag);
+        flag=0;
+      }
+    }
+    setGridArr(auxArr);
   }, [noRows, noColumns]);
 
-  useEffect(() => {
-    setGridArr([gridArrSize]);
-    console.log(`gridArrSize ${gridArrSize}`);
-  }, [gridArrSize]);
-
-  let auxGridArr=[];
-  auxGridArr[0]=0;
-  auxGridArr[1]=1;
-  auxGridArr[2]=0;
-  auxGridArr[3]=1;
-  auxGridArr[4]=0;
-  auxGridArr[5]=1;
-  auxGridArr[6]=0;
-  auxGridArr[7]=1;
-  auxGridArr[8]=0;
-
-  return (
-    <div className='App'>
-
-      <h1 style={styles.center}>Green vs. Red</h1>
-
-      <div>
-          <div style={styles.center}>
-
-            <RowInput  handleChange={handleRowChange} />
-            <ColumnInput  handleChange={handleColumnChange} />
-          </div>
-          <Grid gridArr={auxGridArr} columns={noColumns}/>
+  if (gridArr.length!==0){
+    return (
+      <div className='App'>
+  
+        <h1 style={styles.center}>Green vs. Red</h1>
+  
+        <div>
+            <div style={styles.center}>
+              <RowInput  handleChange={handleRowChange} />
+              <ColumnInput  handleChange={handleColumnChange} />
+            </div>
+            <Grid gridArr={gridArr} columns={noColumns}/>
+        </div>
+  
       </div>
+    );
 
-    </div>
-  );
+  }
+  
 }
 
 const styles = {
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -14,16 +14,25 @@ export default function Grid( { gridArr, columns } ) {
             alignItems: 'center',
             flexWrap: 'wrap',
             justifyContent: 'flex-start',
-            width: gridWidth, // Each cell has a 70px including its margins. The number of columns must be calculated based on that.
+            width: gridWidth, // Each cell has a 70px width including its margins. The number of columns must be calculated based on that.
           }
       };
+
+    let indexes=[0];
+
+    for (let i=0;i<gridArr.lenght; i++) {
+        indexes.push(i);
+    };
+
+    let j=0;
 
     return (
         <div>
             <div className='container' style={styles.grid}>
                 {gridArr.map(element => {
-                    if(element==1){color='red'}else{color='green'};
-                    return <Cell color={color}/>;
+                    if(element===1){color='red'}else{color='green'};
+                    j++;
+                    return <Cell key={j} color={color}/>;
                 })}
             </div>
         </div>
